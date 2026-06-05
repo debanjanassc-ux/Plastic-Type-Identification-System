f01b982d5c7d15e7718eeb30ed82e932f9fbb0db
debanjana sarkar <debanjana.ssc@gmail.com>
2026-06-05 12:38:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -780,7 +780,7 @@ def show_home():
 
         with col_img:
             # Standalone image without double container margins to reduce white space
-            st.image(eco_hero_path, use_container_width=True)
+            st.image("C:\Users\Admin\Desktop\PlasticTtype Identification System\frontend2\assets\eco_hero.png", use_container_width=True)
 
     # Dynamic Ocean Plastic Counter Card
     current_time = time.time()
